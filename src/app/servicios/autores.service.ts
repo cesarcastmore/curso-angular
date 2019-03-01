deba6c948a918c2c742dd3a5f393940c57458df7
ccastillo_tegik <ccastillo@tegik.com>
2019-02-28 20:26:58 -0600

    se define el metodo actualiza del api

--- src/app/servicios/autores.service.ts
+++ src/app/servicios/autores.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Autor} from '../modelos/autor';
+import { Autor } from '../modelos/autor';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,23 +8,28 @@ import { Observable } from 'rxjs';
 })
 export class AutoresService {
 
-public url:string="https://angular-1f48b.firebaseio.com/autores";
+  public url: string = "https://angular-1f48b.firebaseio.com/autores";
 
   constructor(private http: HttpClient) {
 
 
-   }
+  }
 
 
-   public crear(autor: Autor): Observable<any>{
-   	return this.http.post(this.url +".json", autor);
-   }
+  public crear(autor: Autor): Observable < any > {
+    return this.http.post(this.url + ".json", autor);
+  }
 
-   public obtenerAutores(): Observable<any>{
-   	return this.http.get(this.url+ ".json");
-   }
+  public obtenerAutores(): Observable < any > {
+    return this.http.get(this.url + ".json");
+  }
 
-   public borrarAutor(id: string): Observable<any>{
-   	return this.http.delete(this.url + '/' + id + ".json" );
-   }
+  public borrarAutor(id: string): Observable < any > {
+    return this.http.delete(this.url + '/' + id + ".json");
+  }
+
+  public actualizarAutor(autor: Autor): Observable < any > {
+    return this.http.put(this.url + '/' + autor.id + ".json", autor);
+
+  }
 }
